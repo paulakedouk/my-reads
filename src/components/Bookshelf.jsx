41f1bcb2d5b306aafda29f58dd7c78bf54668d2a
paulakedouk <paulakedouk@gmail.com>
2017-07-19 14:47:02 -0700

    Functional Components

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import IndividualBook from './IndividualBook';
 
-class Bookshelf extends Component {
-  render() {
-    let dataBook = this.props.dataBook;
-    let shelf = this.props.shelf;
-    let title = this.props.title;
+function Bookshelf(props) {
+  let dataBook = props.dataBook;
+  // let shelf = props.shelf;
+  let title = props.title;
 
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">
-          {title}
-        </h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {dataBook.map(book => <IndividualBook book={book} key={book.id} />)}
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">
+        {title}
+      </h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {dataBook.map(book => <IndividualBook book={book} key={book.id} />)}
+        </ol>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Bookshelf;
--- src/components/IndividualBook.jsx
+++ src/components/IndividualBook.jsx
@@ -1,43 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class IndividualBook extends Component {
-  render() {
-    let book = this.props.book;
-    // console.log(this.props.dataBook);
-    return (
-      <div>
-        <li className="book">
-          <div className="book-top">
-            <div
-              className="book-cover"
-              style={{
-                width: 128,
-                height: 193,
-                backgroundImage: `url(${book.imageLinks.thumbnail})`
-              }}
-            />
-            <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>
-                  Move to...
-                </option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
+function IndividualBook(props) {
+  // console.log(this.props.dataBook);
+  let book = props.book;
+  return (
+    <div>
+      <li className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${book.imageLinks.thumbnail})`
+            }}
+          />
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
           </div>
-          <div className="book-title">
-            {book.title}
-          </div>
-          <div className="book-authors">
-            {book.authors}
-          </div>
-        </li>
-      </div>
-    );
-  }
+        </div>
+        <div className="book-title">
+          {book.title}
+        </div>
+        <div className="book-authors">
+          {book.authors}
+        </div>
+      </li>
+    </div>
+  );
 }
 
 export default IndividualBook;
